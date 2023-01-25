8d50c45b97b7456aa339bb740fcc2a1fe8896678
Mike V Lvov <Mike.V.Lvov@Gmail.com>
2023-01-25 21:29:59 +0300

    добавил поля анкеты

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 name = input('Введите своё имя ')
 surname = input("Введите свою фамилию ")
-print(name, surname)
+hobby = input("Введите свое хобби ")
+age = input("введите свой возраст ")
+
+print("--------------------")
+print("вот что получилось:)")
+
+print("тебя зовут", name, surname, ", твое хобби", hobby,", твой возраст", age)
