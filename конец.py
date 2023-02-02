5727af1bd4fdffacc31ef3c419cb0b521c8ee015
mike-v-lvov <112233@GhL>
2023-02-02 18:01:24 +0300

    print anketa to game

--- конец.py
+++ конец.py
@@ -1,5 +1,16 @@
+import time
+
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-text = input("введите текст котрый надо отправить: ")
-mc.postToChat(text)
+name = input('Введите своё имя ')
+surname = input("Введите свою фамилию ")
+hobby = input("Введите свое хобби ")
+age = input("введите свой возраст ")
+
+stroka = "В игру пришел", name, surname, ", любит", hobby,", возраст", age
+
+time.sleep(7)
+
+# mc.postToChat("В игру пришел", name, surname, ", любит", hobby,", возраст", age)
+mc.postToChat(stroka)
