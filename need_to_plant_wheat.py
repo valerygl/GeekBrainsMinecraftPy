c695e6c671be15b86690338c2c3f7cd7bd4865d5
ValeryGL <v_lvov@mail.ru>
2023-02-15 20:08:10 +0300

    creat a square of wheat

--- need_to_plant_wheat.py
+++ need_to_plant_wheat.py
@@ -4,6 +4,18 @@ import time
 
 wheat = 296
 
+# делаем площадку, где точно есть пшеница
+time.sleep(5)
+x, y, z = mc.player.getTilePos()
+mc.setBlocks(x - 10, y - 1, z - 10, x + 10, y - 1, z + 10, wheat)
+
+im_here = "Вот твои координаты: Х=" + str(x) + ", У=" + str(y) + ", Z=" + str(z)
+mc.postToChat(im_here)  # печатаю в чат
+print(im_here)  # печатаю то же самое в консоль
+mc.postToChat("в PyCharm и введи _примерно_ эти Х и Z и точно эту же Y!")
+
+
+
 x1 = input("введи x 1")
 y1 = input("введи y 1")
 z1 = input("введи z 1")
