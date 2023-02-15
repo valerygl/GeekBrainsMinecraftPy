578e149fb5161cefa6b48c181c9c989e8641953d
Mike V Lvov <Mike.V.Lvov@Gmail.com>
2023-02-15 18:55:13 +0300

    проверяем, что игрок под водой

--- is_player_in_water.py
+++ is_player_in_water.py
@@ -5,14 +5,15 @@ import time
 time.sleep(8)
 x, y, z = mc.player.getTilePos()
 
-block_type = mc.getBlock(x, y, z)
+# block_type = mc.getBlock(x, y, z)
 
+above = mc.getBlock(x, y + 2, z)
 water = 8
-in_water = block_type == water
+in_water = above == water
 
 
 if in_water:
-    mc.postToChat("игрок в воде")
-else:
-    mc.postToChat("игрок не в воде, он тут: ")
-    mc.postToChat(block_type)
+    mc.postToChat("игрок под водой")
+# else:
+#     mc.postToChat("игрок не в воде, он тут: ")
+#     mc.postToChat(above)
