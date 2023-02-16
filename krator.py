45b418e667b1c4fe637a3c5001fb2179df7a8bd9
mike-v-lvov <112233@GhL>
2023-02-16 18:32:51 +0300

    добавил else

--- krator.py
+++ krator.py
@@ -4,10 +4,17 @@ from mcpi.minecraft import Minecraft
 
 mc = Minecraft.create()
 
-time.sleep(10)
+x, y, z = mc.player.getTilePos()
+
 
 answer = input("создать кратор? y/n")
+time.sleep(10)
+
 if answer == "y":
-    x, y, z = mc.player.getTilePos()
+
     mc.setBlocks(x + 5, y + 5, z + 5, x -5, y - 60, z - 5, 0)
     mc.postToChat("бу бах")
+    print("Crater")
+else:
+    mc.player.setPos(x + 20, y + 4, z + 20)
+    print("teleport")
