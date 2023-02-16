1fbd3e09ea606cfed0169af6e2e2d102dcc35ece
mike-v-lvov <112233@GhL>
2023-02-16 19:40:53 +0300

    добавили ЕСЛИНЕ

--- krator.py
+++ krator.py
@@ -12,12 +12,13 @@ answer = input("создать кратор? y/n ")
 time.sleep(10)
 
 if answer == "y":
-
     mc.setBlocks(x + 5, y + 5, z + 5, x -5, y - 60, z - 5, 0)
     mc.postToChat("бу бах")
     print("Crater")
-else:
+elif answer == "n":
     x += random.randint(-100, 100)
     z += random.randint(-100, 100)
-    mc.player.setPos(x , y + 10, z )
+    mc.player.setPos(x , y + 10, z)
     print("teleport")
+else:
+    print("вы ошиблись")
