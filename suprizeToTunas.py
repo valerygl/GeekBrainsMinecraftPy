6e25157d43744ffc1fa66383d2486aebcf7d3498
mike-v-lvov <112233@GhL>
2023-02-23 17:47:40 +0300

    подарок по выбору

--- suprizeToTunas.py
+++ suprizeToTunas.py
@@ -1,16 +1,10 @@
 import random
 import time
-
 from mcpi.minecraft import Minecraft
-
 mc = Minecraft.create()
 
-time.sleep(5)
-
 answer = input("получить подарок? y/n")
-
-
-answer1 = input("введите что хотите получить")
+answer1 = int(input("введите что хотите получить"))
 
 
 
@@ -19,17 +13,19 @@ if answer == "y":
     x, y, z = mc.player.getTilePos()
     mc.setBlocks(x - 2, y, z + 2, x + 2, y, z + 6, 1)
     mc.setBlocks(x - 1, y + 1, z + 3, x + 1, y + 1, z + 5, 1)
-    mc.setBlock(x, y + 2, z + 4, 56)
+    mc.setBlock(x, y + 2, z + 4, answer1)
 
+    typeTop = mc.getBlock(x, y + 2, z + 4)
+    is_good_suprise = typeTop == 56
+    time.sleep(5)
 
-
-
-elif answer1 == "n":
-    print("не хотите как хотите")
-
+    if is_good_suprise:
+        mc.postToChat("поздравляем")
+    else:
+        mc.postToChat("простите другого нет")
 
 
 
 
 else:
-    print("простите другого нет:(")
+    print("не хотите как хотите")
