b54b40bdd5969bb380bd47623cd8104b7db06bb0
mike-v-lvov <112233@GhL>
2023-03-02 18:26:55 +0300

    колонна с блоками из массива

---
+++ backpack.py
@@ -0,0 +1,21 @@
+import time
+from mcpi.minecraft import Minecraft
+
+mc = Minecraft.create()
+
+my_items = [15, 121, 57, 65, 2]
+x, y, z = mc.player.getTilePos()
+
+
+mc.setBlock(x, y, z, my_items[0])
+mc.setBlock(x, y+1, z, my_items[1])
+mc.setBlock(x, y+2, z, my_items[2])
+mc.setBlock(x, y+3, z, my_items[3])
+mc.setBlock(x, y+4, z, my_items[4])
+
+
+
+
+
+
+
