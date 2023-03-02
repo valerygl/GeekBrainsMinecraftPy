a5579f9164ff6f5d125febbd94bc7dbe6c17f0f5
mike-v-lvov <112233@GhL>
2023-03-02 18:33:24 +0300

    пишет в чат айди блока

--- backpack.py
+++ backpack.py
@@ -6,13 +6,17 @@ mc = Minecraft.create()
 my_items = [15, 121, 57, 65, 2]
 x, y, z = mc.player.getTilePos()
 
-
+time.sleep(3)
 mc.setBlock(x, y, z, my_items[0])
+mc.postToChat(my_items[0])
 mc.setBlock(x, y+1, z, my_items[1])
+mc.postToChat(my_items[1])
 mc.setBlock(x, y+2, z, my_items[2])
+mc.postToChat(my_items[2])
 mc.setBlock(x, y+3, z, my_items[3])
+mc.postToChat(my_items[3])
 mc.setBlock(x, y+4, z, my_items[4])
-
+mc.postToChat(my_items[4])
 
 
 
