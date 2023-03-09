16a211cc2f6889449a53b0931c20ac5d5c9fe092
Mike V Lvov <Mike.V.Lvov@Gmail.com>
2023-03-09 19:46:26 +0300

    возможно находит алмазы

---
+++ diamondFiend.py
@@ -0,0 +1,13 @@
+from mcpi.minecraft import Minecraft
+
+mc = Minecraft.create()
+x, y, z = mc.player.getTilePos()
+
+for block_y in range(y, -64, -1):
+    block = mc.getBlock(x, block_y, z)
+    print(block)
+    if block == 56:
+        mc.postToChat(f"алмаз есть?: {block_y}")
+        break
+else:
+    mc.postToChat("алмаза нет")
