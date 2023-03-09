cc3e6d73c4d62451b37a22825c652cfb19412881
Mike V Lvov <Mike.V.Lvov@Gmail.com>
2023-03-09 19:46:50 +0300

    рандомный рюкзак

---
+++ random_backpack.py
@@ -0,0 +1,10 @@
+import random
+
+my_items = []
+
+for number in range(300):
+    my_items.append(random.randint(30, 70))
+    if new_items == 56:
+        print("никаких алмазов")
+
+    print(my_items)
