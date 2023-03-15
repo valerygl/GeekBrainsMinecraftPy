01f35d45a1ec5baa9b1cdf2c5a70c85e6fe09f99
Mike V Lvov <Mike.V.Lvov@Gmail.com>
2023-03-15 19:06:56 +0300

    ищет алмазы с id 56 или 633

--- diamondFiend.py
+++ diamondFiend.py
@@ -6,8 +6,8 @@ x, y, z = mc.player.getTilePos()
 for block_y in range(y, -64, -1):
     block = mc.getBlock(x, block_y, z)
     print(block)
-    if block == 56:
+    if block == 56 or block == 633:
         mc.postToChat(f"алмаз есть?: {block_y}")
         break
-else:
-    mc.postToChat("алмаза нет")
+    else:
+        mc.postToChat("алмаза нет")
